15ae56e055b79f52865d56b5e8d66df5d36415fa
laon <reeedt@gmail.com>
2021-04-17 15:57:58 -0400

    Saving, clearing, and loading features were added. json file path is hardcoded.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter.messagebox import askyesno
 from tkinter import font
-
+import json
 
 class Activity_Input(tk.Frame):
     def __init__(self, parent, label, default=""):
@@ -20,7 +20,8 @@ class Activity_Input(tk.Frame):
         self.enter_button.pack(side='left')
 
     def entered(self):
-        self.parent.add_activity(self.new_activity)
+        self.parent.add_activity(self.new_activity.get())
+        
 
 class Activity_Listing(tk.Frame):
     def __init__(self, parent, new_activity=''):
@@ -33,11 +34,10 @@ class Activity_Listing(tk.Frame):
         self.default_font = font.Font(family='Courier', size=10, weight=font.NORMAL, overstrike=0)
         self.striked_font = font.Font(family='Courier', size=10, weight=font.NORMAL, overstrike=1)
 
-
         self.number = tk.Label(self, text='1')
         self.check_box = tk.Checkbutton(self, variable=self.checked, onvalue=1, offvalue=0, command=self.check_box_checked)
         self.label = tk.Label(self, text=new_activity, font=self.default_font)
-        self.cross_button = tk.Button(self, text="Cross off", command=self.cross_off)
+        self.cross_button = tk.Button(self, text="Cross off", command=self.toggle_crossed_off)
         self.delete_button = tk.Button(self, text="delete", command=self.delete_self)
 
         self.check_box.pack(side="left")
@@ -46,86 +46,119 @@ class Activity_Listing(tk.Frame):
         self.delete_button.pack(side="right")
         self.cross_button.pack(side="right")
 
-    def cross_off(self):
+    def toggle_crossed_off(self):
         if self.crossed_off:
-            self.label.configure(font=self.default_font)
-            self.crossed_off = False
+            self.set_crossed_off(False)
         else:
+            self.set_crossed_off(True)
+
+    def set_crossed_off(self, crossed):
+        if crossed:    
             self.label.configure(font=self.striked_font)
             self.crossed_off = True
-        
+        else:
+            self.label.configure(font=self.default_font)
+            self.crossed_off = False
+
 
     def delete_self(self):
         answer = askyesno(title="Deletion Inquiry", message="Delete this activity?")
         if answer:
-            print(self.parent)
             self.parent.destroy_listing(int(self.number.cget('text')))
             self.parent.update_activity_list()
-            
         else:
             return
 
     def check_box_checked(self):
         print(self.checked.get())
 
-class Activities_Frame(tk.Frame):
+class Activities_Container(tk.Frame):
     def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-
+        tk.Frame.__init__(self, parent) 
         self.activity_input = Activity_Input(self, "input", default="Input activity")
-
         self.activity_list = []
         self.activity_input.pack(side="top", fill=tk.X)
 
-    def add_activity(self, new_activity):
-        print(new_activity.get())
-        activity = Activity_Listing(self, new_activity=new_activity.get())
+    def add_activity(self, new_activity, crossed_off=False):
+        activity = Activity_Listing(self, new_activity=new_activity)
         self.activity_list.append(activity)
         activity.number.config(text=str(len(self.activity_list)))
+        activity.set_crossed_off(crossed_off)
         activity.pack(side="top", fill=tk.X)
-        
 
+    def clear_listings(self):
+        for _i in range(len(self.activity_list)):
+            self.activity_list[-1].destroy()
+            self.activity_list.pop(-1)
+        self.update_activity_list()
+ 
     def update_activity_list(self):
         for i in range(len(self.activity_list)):
-            if self.activity_list[i]:
-                pass
-                #print('Success ', self.activity_list[i])
+            if self.activity_list[i]: 
                 self.activity_list[i].number.config(text=str(i+1))
             else:
                 print("This listing doesnt exist")
 
     def destroy_listing(self, listing_number):
-        self.activity_list[listing_number - 1].destroy()
-        self.activity_list.pop(listing_number - 1)
-        
+        self.activity_list[listing_number].destroy()
+        self.activity_list.pop(listing_number) 
         self.update_activity_list()
+
+    def export_json_dict(self):
+        dictionary = {}
+        for activity in self.activity_list:
+            dictionary[activity.number.cget('text')] = [activity.label.cget('text'), activity.crossed_off]
+        with open("test.json", "w") as outfile:
+            json.dump(dictionary, outfile)
+
+    def import_json_dict(self):
+        json_dict = open('test.json')
+        loaded_dict = json.load(json_dict)
+        self.load_dict(loaded_dict)
+
+    def load_dict(self, loaded_dict):
+        self.clear_listings()
+        for i in range(len(loaded_dict.keys())):
+            self.add_activity(loaded_dict[str(i+1)][0], loaded_dict[str(i+1)][1])
+
 
 class UI_Frame(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
      
         self.label = tk.Label(self, text="To-do list")
-        self.listing_frame = Activities_Frame(self)
+        self.activities_container = Activities_Container(self)
+        self.save_button = tk.Button(text="Save", command=self.save_list)
+        self.load_button = tk.Button(text="load", command=self.load_list)
+        self.clear_button = tk.Button(text="Clear", command=self.clear_listings)
+
 
         self.label.pack(side="top")
-        self.listing_frame.pack(side="top", fill=tk.X)
+        self.activities_container.pack(side="top", fill=tk.X)
+        self.save_button.pack(side="bottom")
+        self.load_button.pack(side="bottom")
+        self.clear_button.pack(side="bottom")
+
+
+    def save_list(self):
+        self.activities_container.export_json_dict()
+
+    def load_list(self):
+        self.activities_container.import_json_dict()
+
+    def clear_listings(self):
+        self.activities_container.clear_listings()
+        
 
 
 class To_Do():
     def __init__(self):
         self.root = tk.Tk()
-        self.root.geometry("320x500")
-
+        self.root.geometry("600x500")
         self.new_activity = tk.StringVar()
-
         self.ui_frame = UI_Frame(self.root)
-
         self.ui_frame.pack(side="top", fill=tk.X)
-
-
         self.root.mainloop()
-        
-
 
 if __name__=="__main__":
     app = To_Do()
